ca9218d0f4c22fcedab0fa89c9c77e6068cb3722
Mikko Alfthan <malfthan@gmail.com>
2018-10-17 20:29:43 +0300

    Ramp test

--- motortest2.py
+++ motortest2.py
@@ -12,6 +12,35 @@ pi = pigpio.pi()
 pi.set_mode(DIR, pigpio.OUTPUT)
 pi.set_mode(STEP, pigpio.OUTPUT)
 
+def generate_ramp(ramp):
+    """Generate ramp wave forms.
+    ramp:  List of [Frequency, Steps]
+    """
+    pi.wave_clear()     # clear existing waves
+    length = len(ramp)  # number of ramp levels
+    wid = [-1] * length
+
+    # Generate a wave per ramp level
+    for i in range(length):
+        frequency = ramp[i][0]
+        micros = int(500000 / frequency)
+        wf = []
+        wf.append(pigpio.pulse(1 << STEP, 0, micros))  # pulse on
+        wf.append(pigpio.pulse(0, 1 << STEP, micros))  # pulse off
+        pi.wave_add_generic(wf)
+        wid[i] = pi.wave_create()
+
+    # Generate a chain of waves
+    chain = []
+    for i in range(length):
+        steps = ramp[i][1]
+        x = steps & 255
+        y = steps >> 8
+        chain += [255, 0, wid[i], 255, 1, x, y]
+
+    pi.wave_chain(chain)  # Transmit chain.
+
+
 # Set up input switch
 #pi.set_mode(SWITCH, pigpio.INPUT)
 #pi.set_pull_up_down(SWITCH, pigpio.PUD_UP)
@@ -27,13 +56,20 @@ pi.set_mode(STEP, pigpio.OUTPUT)
 #    pi.write(MODE[i], RESOLUTION['Full'][i])
 
 # Set duty cycle and frequency
-pi.set_PWM_dutycycle(STEP, 128)  # PWM 1/2 On 1/2 Off
-pi.set_PWM_frequency(STEP, 500)  # 500 pulses per second
+#pi.set_PWM_dutycycle(STEP, 128)  # PWM 1/2 On 1/2 Off
+#pi.set_PWM_frequency(STEP, 500)  # 500 pulses per second
 
 try:
     while True:
         pi.write(DIR, 0)  # Set direction
         sleep(.1)
+    # Ramp up
+    generate_ramp([[320, 200],
+    	       [500, 200],
+	       [800, 200],
+	       [1000, 200],
+	       [1600, 200],
+	       [2000, 200]])        
 
 except KeyboardInterrupt:
     print ("\nCtrl-C pressed.  Stopping PIGPIO and exiting...")
