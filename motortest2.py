58c190023ebe695444ab1e1b33dc880435912f5c
Mikko Alfthan <malfthan@gmail.com>
2018-10-18 11:39:57 +0300

    Cleanup

--- motortest2.py
+++ motortest2.py
@@ -3,7 +3,6 @@ import pigpio
 
 DIR = 2     # Direction GPIO Pin
 STEP = 3    # Step GPIO Pin
-#SWITCH = 16  # GPIO pin of switch
 
 # Connect to pigpiod daemon
 pi = pigpio.pi()
@@ -41,24 +40,6 @@ def generate_ramp(ramp):
     pi.wave_chain(chain)  # Transmit chain.
 
 
-# Set up input switch
-#pi.set_mode(SWITCH, pigpio.INPUT)
-#pi.set_pull_up_down(SWITCH, pigpio.PUD_UP)
-
-#MODE = (14, 15, 18)   # Microstep Resolution GPIO Pins
-#RESOLUTION = {'Full': (0, 0, 0),
-#              'Half': (1, 0, 0),
-#              '1/4': (0, 1, 0),
-#              '1/8': (1, 1, 0),
-#              '1/16': (0, 0, 1),
-#              '1/32': (1, 0, 1)}
-#for i in range(3):
-#    pi.write(MODE[i], RESOLUTION['Full'][i])
-
-# Set duty cycle and frequency
-#pi.set_PWM_dutycycle(STEP, 128)  # PWM 1/2 On 1/2 Off
-#pi.set_PWM_frequency(STEP, 500)  # 500 pulses per second
-
 pi.write(DIR, 0)  # Set direction
 
 # Ramp forwards
@@ -71,19 +52,4 @@ generate_ramp([[320, 50],
 	[320, 50]
 ])
 
-#sleep(1)
-
-#pi.write(DIR, 1)  # Set direction
-
-# Ramp backwards
-#generate_ramp([[320, 50],
-#	[500, 100],
-#	[800, 200],
-#	[1000, 4600],
-#	[800, 200],
-#	[500, 100],
-#	[320, 50]
-#])
-
-pi.set_PWM_dutycycle(STEP, 0)  # PWM off
 pi.stop()
